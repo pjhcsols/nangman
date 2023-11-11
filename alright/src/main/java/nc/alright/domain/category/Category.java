cc8578b45dd65edf0091c6521caa3dd04950b10b
pjhcsols <pjhcsols@naver.com>
2023-11-12 08:50:09 +0900

    Feat: Category domain 추가

---
+++ alright/src/main/java/nc/alright/domain/category/Category.java
@@ -0,0 +1,41 @@
+package nc.alright.domain.category;
+
+public class Category {
+    private Long categoryId; //productcategoryId와 연결해야됨
+    private String categoryName;
+    // private Long parentCategoryId;
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        //this.parentCategoryId = parentCategoryId;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    /*
+    public Long getParentCategoryId() {
+        return parentcategoryId;
+    }
+
+    public void setParentCategoryId(Long parentCategoryId) {
+        this.parentcategoryId = parentCategoryId;
+    }
+
+     */
+}
+
