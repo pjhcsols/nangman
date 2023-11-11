ad5226f479183e4fde5b0899383ed02c142495f7
MINSU LEE <minsoo030232@gmail.com>
2023-11-11 10:26:52 +0900

    Feat: FE 초기화

---
+++ fe_alright/src/routes.js
@@ -0,0 +1,11 @@
+import App from "./App";
+import LoginPage from "./pages/LoginPage";
+
+const routes = [
+    {
+        path : '/login',
+        component : LoginPage
+    }
+]
+
+export default routes;
