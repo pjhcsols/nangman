4e728bb37681394ec1e02150194f609eb68521f6
pjhcsols <110244523+pjhcsols@users.noreply.github.com>
2023-11-11 09:30:12 +0900

    Feat: BE 초기화

---
+++ alright/src/main/java/nc/alright/AlrightApplication.java
@@ -0,0 +1,13 @@
+package nc.alright;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlrightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlrightApplication.class, args);
+	}
+
+}
---
+++ alright/src/test/java/nc/alright/AlrightApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.alright;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlrightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
