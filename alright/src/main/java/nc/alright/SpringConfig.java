4eda348b148531694db2659e9f260ca15eb18f14
pjhcsols <pjhcsols@naver.com>
2023-11-11 10:27:33 +0900

    Feat: User domain&repository&service&controller 생성 및 추가

---
+++ alright/src/main/java/nc/alright/SpringConfig.java
@@ -0,0 +1,4 @@
+package nc.alright;
+
+public class SpringConfig {
+}
--- alright/src/main/java/nc/alright/domain/user/User.java
+++ alright/src/main/java/nc/alright/domain/user/User.java
@@ -1,4 +1,74 @@
 package nc.alright.domain.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+    private String userEmail;
+    private String userPhoneNumber;
+    private String password;
+    private Date createTime;
+    private String userName;
+
+
+    public User() {
+
+    }
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
 }
